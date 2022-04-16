c5dd88614992362085df0449eaf5a20b525bada2
AadiYNWA <97405355+AadiYNWA@users.noreply.github.com>
2022-04-16 22:08:08 +0530

    Update mapdata.js

--- mapdata.js
+++ mapdata.js
@@ -74,99 +74,98 @@ var simplemaps_countrymap_mapdata={
   state_specific: {
     "1": {
       name: "Andaman And Nicobar",
-      description: "default",
+      description: "The Andaman and Nicobar Islands, located in the east of the Indian mainland geographically, float in splendid isolation in the Bay of Bengal. Once a hill range extending from Myanmar to Indonesia, these picturesque undulating islands, islets numbering around 836, are covered with dense rain-fed, damp and evergreen forests and endless varieties of exotic flora and fauna.",
       color: "yellow",
       hover_color: "default",
       url: "default"
     },
     "2": {
       name: "Andhra Pradesh",
-      description: "default",
+      description: "Andhra Pradesh is known as Koh-Noor of India and there are many places to visit in this beautiful state. It is famous for its rich natural resources, rivers, historical monuments, temple and much more.",
       color: "GreenYellow",
       hover_color: "default",
       url: "default"
     },
     "3": {
       name: "Arunachal Pradesh",
-      description: "default",
+      description: "Popularly known as ‘Land of the Dawn-lit-Mountains’, Arunachal Pradesh is India’s remotest state and the first Indian soil to greet the rising sun. Located on the northeastern tip of India, this beautiful land is endowed with dazzling array of flora and fauna that is sure to allure any tourist. The misty hills, sparkling rivers, gurgling waterfalls add charm to the beauty of this incredible land. Arunachal Pradesh is dotted with innumerable historical monuments and archeological remains, which bear testimony to its rich cultural heritage.",
       color: "HotPink",
       hover_color: "default",
       url: "default"
     },
     "4": {
       name: "Assam",
-      description: "default",
+      description: "Assam, located in the North-East of India, is one of the most sought after destinations in the country because of its magnificent tourism products like exotic wildlife, awesome scenic beauty, colorful fairs and festivals, age old historical monuments, lush green tea gardens and golf courses, massive river Brahmaputra and its tributaries, serene and tranquil country side and warm and hospitable people.",
       color: "MediumPurple",
       hover_color: "default",
       url: "default"
     },
     "5": {
       name: "Bihar",
-      description: "default",
+      description: "Bihar is a state full of history and spiritual significance. Located in the eastern part of India, this state is known as the land of monasteries. Described as the birthplace of Buddhism and the home to the two greatest Indian dynasties, the Mauryas and the Guptas, Bihar is no less than an 'Uphar' to the land of India.",
       color: "orange",
       hover_color: "default",
       url: "default"
     },
     "6": {
       name: "Chandigarh",
-      description: "default",
+      description: "Chandigarh is an Union Territory that serves as the capital to the states of Punjab and Haryana. Planned by the Swiss-French architect Le Corbusier, it is considered one of the most well planned cities in the world and ranked the happiest city in India. Its Capitol Complex was declared as an UNESCO World Heritage site in 2016. ",
       color: "orange",
       hover_color: "default",
       url: "default"
     },
     "7": {
       name: "Chhattisgarh",
-      description: "default",
+      description: "Chhattisgarh, situated in the heart of India, is endowed with a rich cultural heritage and attractive natural diversity. The state is full of ancient monuments, rare wildlife, exquisitely carved temples, Buddhist sites, palaces, waterfalls, caves, rock paintings, and hill plateaus. Mainpat is mini Shimla of Chhattisgarh. There are many waterfalls, hot springs, caves, temples, dams and national parks, tiger reserves and wildlife sanctuaries in Chhattisgarh. India's first man-made jungle safari is also situated in Raipur.",
       color: "yellow",
       hover_color: "default",
       url: "default"
     },
     "8": {
       name: "Dadra And Nagar Haveli",
-      description: "default",
+      description: "Endowed with nature’s most scenic gifts, Dadra & Nagar Haveli offers days that start with the rustling sounds of palm trees and finish with orange sunsets. The Western Ghats range rise to the east, and the foothills of the range occupy the eastern portion of the district. While the territory is landlocked, the Arabian Sea lies just to the west in Gujarat. ",
       color: "default",
       hover_color: "default",
       url: "default"
     },
     "9": {
       name: "Daman And Diu",
-      
-      description: "default",
+      description: "The ex-Portuguese enclave of Daman (Damao) is a world of its own. It is a paradise for travelers searching for peace, solitude and serenity. The charm of this place lies in its fine forts and churches, in which you can get a whiff of Portuguese preeminence. The island of Diu is the blissful confluence of silver sands and the golden sun.  It’s replete with majestic beaches, whitewashed fishing cottages and excellent roads for you to ride along the coast with the wind blowing in your hair.",
       color: "default",
       hover_color: "default",
       url: "default"
     },
     "10": {
       name: "Delhi",
-      description: "default",
+      description: "The National Capital, Delhi is just too dynamic to describe in a few words. It’s rich history saturated with old stories reflects in beautiful heritage buildings tucked around the sprawling city. The dusty patina of the old stonewalls gives a rumbling beauty to the city landscape and one can almost hear the sounds of the past echoing.",
       color: "green",
       hover_color: "default",
       url: "default"
     },
     "11": {
       name: "Goa",
-      description: "default",
+      description: "Goa is a state in western India with coastlines stretching along the Arabian Sea. Its long history as a Portuguese colony prior to 1961. Goa is also known for its beaches, ranging from popular stretches at Baga and Palolem to those in laid-back fishing villages such as Agonda.",
       color: "grey",
       hover_color: "default",
       url: "default"
     },
     "12": {
       name: "Gujarat",
-      description: "default",
+      description: "Stretches out into the Arabian Sea, with a hint of the desert and with a coastline of 1600 kms long is Gujarat – the home state of Mahatma Gandhi, the Father of Nation. It is renowned for its beaches, temple towns and historic capitals. Wildlife sanctuaries, hill resorts and natural grandeur are gifts of Gujarat. Sculpture, handicrafts, arts, festivals also make the state rich.",
       color: "GreenYellow",
       hover_color: "default",
       url: "default"
     },
     "13": {
       name: "Haryana",
-      description: "default",
+      description: "The name Haryana means the abode of God. It represents the face of modern India and is a vivid kaleidoscope of diverse landscapes, showcasing magnificent archaeology and celebrating art and culture. From being referred to as ‘heaven on earth’ in ancient Sanskrit texts to being the bedrock of Indus valley civilization, Haryana has one of the most unique histories.",
       color: "MediumPurple",
       hover_color: "default",
       url: "default"
     },
     "14": {
       name: "Himachal Pradesh",
-      description: "default",
+      description: "Host to the Dalai Lama, Himachal Pradesh has a strong Tibetan presence, reflected in its Buddhist temples and monasteries, as well as its vibrant Tibetan New Year celebrations. The Himalayas are home to scenic mountain towns and resorts in Manali and Dalhousie. The region is also well known for its trekking, climbing and skiing areas.",
       color: "HotPink",
       hover_color: "default",
       url: "default"
