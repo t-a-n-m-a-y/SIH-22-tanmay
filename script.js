f902d00481f5a09cf25d0394d9ec1cc1a492d4ab
Tanmay <tantush007@gmail.com>
2022-04-17 00:50:38 +0530

    final with banner

--- script.js
+++ script.js
@@ -35,3 +35,28 @@ document.addEventListener('click',e=> {
         dropdown.classList.remove('active')
     })
 })
+
+let slideIndex = 0;
+showSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
